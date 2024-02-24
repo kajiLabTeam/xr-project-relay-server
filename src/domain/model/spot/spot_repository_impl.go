3bc98c9f81d3f96f292e3b662639e9264db47b31
Yusei Mizutani <yusei4520030401@gmail.com>
2024-02-25 02:58:40 +0900

    fix : スポットドメインのコレクションを定義

--- src/domain/model/spot/spot_repository_impl.go
+++ src/domain/model/spot/spot_repository_impl.go
@@ -1,13 +1,22 @@
 package spot_model_domain
 
 import (
-	"os"
-
-	"github.com/kajiLabTeam/xr-project-relay-server/config/env"
+	"github.com/kajiLabTeam/xr-project-relay-server/src/config/env"
+	input_dto_infrastructure "github.com/kajiLabTeam/xr-project-relay-server/src/infrastructure/dto/input"
 )
 
 type SpotRepository interface {
-	GetSpot(functionServerEnv *env.FunctionServerEnv, spotIds *[]string, rawData *os.File) (*Spot, error)
-	GetAreaSpots(functionServerEnv *env.FunctionServerEnv, radius int, coordinate *Coordinate) (SpotCollection, error)
-	CreateSpot(functionServerEnv *env.FunctionServerEnv, rawDataFile *os.File, s *Spot) (*Spot, error)
+	GetSpotBySpotIdsAndRawDataFileRepository(
+		functionServerEnv *env.FunctionServerEnv,
+		spotIds []string,
+		rawData []byte,
+	) (*Spot, error)
+	GetSpotCollectionByCoordinateAndRadiusRepository(
+		functionServerEnv *env.FunctionServerEnv,
+		radius int,
+		c *Coordinate,
+	) (SpotCollection, error)
+	CreateSpotRepository(
+		csidto *input_dto_infrastructure.CreateSpotRepositoryInputDTO,
+	) (*Spot, error)
 }
