852609bf732af6636d5ddb38bbb0a6afe3582a9b
Yusei Mizutani <yusei4520030401@gmail.com>
2024-03-15 00:08:48 +0900

    add : エントリーポイントの追加

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/kajiLabTeam/xr-project-relay-server/presentation/handlers"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	r := gin.Default()
+
+	handlers.GetObjectBySpotHandler(r)
+	handlers.GetObjectByAreaHandler(r)
+	handlers.CreateObjectHandler(r)
+
+	r.Run(":8080")
+}
