cf4b69167560e04d7f4456c9eec83f5cf14779ae
Yusei Mizutani <yusei4520030401@gmail.com>
2024-03-15 00:09:43 +0900

    add : エリア取得のサービスを定義

---
+++ application/services/get_object_by_area_service.go
@@ -0,0 +1,72 @@
+package services
+
+import (
+	application_models_domain "github.com/kajiLabTeam/xr-project-relay-server/domain/models/application"
+	object_collection_models_domain "github.com/kajiLabTeam/xr-project-relay-server/domain/models/object_collection"
+	user_models_domain "github.com/kajiLabTeam/xr-project-relay-server/domain/models/user"
+	"github.com/kajiLabTeam/xr-project-relay-server/domain/repository_impl"
+)
+
+type GetObjectByAreaService struct {
+	objectRepo repository_impl.ObjectRepositoryImpl
+	spotRepo   repository_impl.SpotRepositoryImpl
+}
+
+func NewGetObjectByAreaService(
+	objectRepo repository_impl.ObjectRepositoryImpl,
+	spotRepo repository_impl.SpotRepositoryImpl,
+) *GetObjectByAreaService {
+	return &GetObjectByAreaService{
+		objectRepo: objectRepo,
+		spotRepo:   spotRepo,
+	}
+}
+
+func (gobas *GetObjectByAreaService) Run(
+	area int,
+	latitude float64,
+	longitude float64,
+	user *user_models_domain.User,
+	application *application_models_domain.Application,
+) (
+	*user_models_domain.User,
+	*object_collection_models_domain.ObjectCollection,
+	error,
+) {
+	// エリア探索を用いて周辺スポットを取得
+	areaSpotCollection, err := gobas.spotRepo.FindForCoordinateAndRadius(
+		area,
+		latitude,
+		longitude,
+		application,
+	)
+	if err != nil {
+		return user, nil, err
+	}
+	// 周辺スポットがない場合
+	if areaSpotCollection == nil {
+		return user, nil, nil
+	}
+
+	// 周辺スポットのIDを取得
+	spotIds := areaSpotCollection.GetSpotIds()
+
+	// 周辺スポットを元にスポットに紐づくオブジェクトを取得
+	areaObject, err := gobas.objectRepo.FindForSpotIds(
+		spotIds,
+		user,
+		application,
+	)
+	if err != nil {
+		return user, nil, err
+	}
+	// 周辺スポットがない場合
+	if areaObject == nil {
+		return user, nil, nil
+	}
+
+	areaObject.LinkSpots(areaSpotCollection)
+
+	return user, areaObject, nil
+
+}
